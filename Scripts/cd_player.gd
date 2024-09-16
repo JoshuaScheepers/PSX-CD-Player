d9c1c5406fe74676861d1689cfcedadb064c7374
ujochi <162288710+JoshuaScheepers@users.noreply.github.com>
2024-09-16 18:36:35 +0200

    Cleaned up and rearranged the script and fixed up some functions

--- Scripts/cd_player.gd
+++ Scripts/cd_player.gd
@@ -1,32 +1,39 @@
 extends Node2D
 
+# Track Management
+var track_files = []
+var original_order = []
+var track_durations = []
+var current_track_index = 0
+
+# Playback Modes
+enum PlaybackMode { NORMAL, SHUFFLE, PROGRAM }
+var current_playback_mode = PlaybackMode.NORMAL
+
+# Shuffle Mode
 var played_tracks = []
 var shuffle_order = []
 var shuffle_history = []
 var current_shuffle_index = -1
 
-var track_files = []
-var current_track_index = 0
-var previous_track_index = 0
+# Program Mode
+var program_list = []
+var current_program_position = 0  # New variable to track position in program_list
 
+# Time Display
 enum TimeMode { ELAPSED, REMAINING_TRACK, REMAINING_TOTAL }
 var time_mode := TimeMode.ELAPSED
-var track_durations := []
 
+# Repeat Mode
 enum RepeatMode { NO_REPEAT, REPEAT_ONE, REPEAT_ALL }
 var repeat_mode := RepeatMode.NO_REPEAT
-var original_order = []
 
-var shuffle = false
-
-enum PlaybackMode { NORMAL, SHUFFLE, PROGRAM }
-var current_playback_mode = PlaybackMode.NORMAL
-var program_list = []
-
+# Seeking
 var seeking_forward = false
 var seeking_backward = false
 var seek_speed = 4
 
+# Shader Time
 var shader_time = 0.0
 
 func _ready():
@@ -36,6 +43,62 @@ func _ready():
 	original_order = track_files.duplicate()
 	calculate_track_durations()
 	update_track_visibility()
+
+func assign_tracks():
+	var button_index = 1
+	for button in get_tree().get_nodes_in_group("track_buttons"):
+		var callable = Callable(self, "_on_track_button_pressed").bind(button_index)
+		button.connect("pressed", callable)
+		button_index += 1
+
+func populate_tracks():
+	track_files.clear()
+
+	# External music folder path
+	var external_music_path = OS.get_executable_path().get_base_dir() + "/Music"
+	var external_music_directory = DirAccess.open(external_music_path)
+	
+	if external_music_directory:
+		print("External playlist loaded")
+		external_music_directory.list_dir_begin()
+		var file_name = external_music_directory.get_next()
+		while file_name != "":
+			if file_name.ends_with(".mp3"):
+				var full_path = external_music_path + "/" + file_name
+				track_files.append(full_path)
+			file_name = external_music_directory.get_next()
+		external_music_directory.list_dir_end()
+	else:
+		# Fallback to internal resources (res://)
+		print("Default playlist loaded")
+		var default_tracks = [
+			"res://Music/01 - Opening the Portal.mp3",
+			"res://Music/02 - Liminal Phasing.mp3",
+			"res://Music/03 - Megadalene.mp3",
+			"res://Music/04 - Closing The Portal (Avec Batterie).mp3"
+		]
+		for track_path in default_tracks:
+			if ResourceLoader.exists(track_path):
+				track_files.append(track_path)
+			else:
+				print("Failed to load internal resource: ", track_path)
+
+	setup_tracks_after_loading()
+
+func setup_tracks_after_loading():
+	if track_files.size() > 0:
+		load_track(0)
+		$AudioStreamPlayer.stop()
+		calculate_track_durations()
+
+func calculate_track_durations():
+	track_durations.clear()
+	for track_path in track_files:
+		var duration = get_track_duration(track_path)
+		if duration >= 0:
+			track_durations.append(duration)
+		else:
+			track_durations.append(0)
 
 func _physics_process(delta):
 	cursor_movement()
@@ -53,67 +116,31 @@ func _physics_process(delta):
 
 func cursor_movement():
 	var focused_node = get_viewport().gui_get_focus_owner()
-	$cursor.global_position = focused_node.global_position + (focused_node.size * 0.5)
-
-func populate_tracks():
-	track_files.clear()
-	track_durations.clear()
-
-	# External music folder path
-	var external_music_path = OS.get_executable_path().get_base_dir() + "/Music"
-	var external_music_directory = DirAccess.open(external_music_path)
-	
-	if external_music_directory:
-		print("External playlist loaded")
-		external_music_directory.list_dir_begin()
-		var file_name = external_music_directory.get_next()
-		while file_name != "":
-			if file_name.ends_with(".mp3"):
-				var full_path = external_music_path + "/" + file_name
-				track_files.append(full_path)
-				track_durations.append(get_track_duration(full_path))
-			file_name = external_music_directory.get_next()
-		external_music_directory.list_dir_end()
-	else:
-		# Fallback to internal resources (res://)
-		print("Default playlist loaded")
-		var default_tracks = [
-			"res://Music/01 - Opening the Portal.mp3",
-			"res://Music/02 - Liminal Phasing.mp3",
-			"res://Music/03 - Megadalene.mp3",
-			"res://Music/04 - Closing The Portal (Avec Batterie).mp3"
-		]
-		for track_path in default_tracks:
-			if ResourceLoader.exists(track_path):
-				track_files.append(track_path)
-				track_durations.append(get_track_duration(track_path))
-			else:
-				print("Failed to load internal resource: ", track_path)
-
-	setup_tracks_after_loading()
-
-func setup_tracks_after_loading():
-	if track_files.size() > 0:
-		load_track(0)
-		$AudioStreamPlayer.stop()
-
-func assign_tracks():
-	var button_index = 1
-	for button in get_tree().get_nodes_in_group("track_buttons"):
-		var callable = Callable(self, "_on_track_button_pressed").bind(button_index)
-		button.connect("pressed", callable)
-		button_index += 1
+	if focused_node:
+		$cursor.global_position = focused_node.global_position + (focused_node.size * 0.5)
 
 func load_track(track_index: int):
-	var track_list = track_files
-	if track_index >= track_list.size():
-		print("Track index out of range.")
-		return
+	if current_playback_mode == PlaybackMode.PROGRAM:
+		# Ensure current_program_position is valid
+		if current_program_position >= program_list.size() or current_program_position < 0:
+			print("Program position out of range.")
+			return
+		track_index = program_list[current_program_position]
+	elif current_playback_mode == PlaybackMode.SHUFFLE:
+		if current_track_index >= track_files.size() or current_track_index < 0:
+			print("Shuffle track index out of range.")
+			return
+		track_index = current_track_index
+	else:
+		if track_index >= track_files.size() or track_index < 0:
+			print("Track index out of range.")
+			return
 
-	var track_path = track_list[track_index]
+	var track_path = track_files[track_index]
 	var stream = load_mp3(track_path)
 	if stream:
 		$AudioStreamPlayer.stream = stream
+		current_track_index = track_index  # Update current_track_index
 		update_current_track_label()
 	else:
 		print("Failed to load stream for track: ", track_path)
@@ -164,35 +191,53 @@ func update_elapsed_time_label():
 	if $AudioStreamPlayer.is_playing():
 		elapsed_time = $AudioStreamPlayer.get_playback_position()
 
-	var remaining_track_time = 0.0
-	var total_time = get_total_remaining_time()
-
 	var minutes = 0
 	var seconds = 0
 
-	if $AudioStreamPlayer.stream and $AudioStreamPlayer.stream.get_length() > 0:
-		remaining_track_time = $AudioStreamPlayer.stream.get_length() - elapsed_time
-		minutes = int(remaining_track_time / 60)
-		seconds = int(remaining_track_time) % 60
-	else:
-		print("Stream is not loaded or is invalid.")
+	if time_mode == TimeMode.REMAINING_TRACK or time_mode == TimeMode.REMAINING_TOTAL:
+		if not $AudioStreamPlayer.stream or $AudioStreamPlayer.stream.get_length() <= 0:
+			print("Stream is not loaded or is invalid.")
+			$time_label.text = "00      00"
+			return
 
 	match time_mode:
 		TimeMode.ELAPSED:
 			minutes = int(elapsed_time / 60)
 			seconds = int(elapsed_time) % 60
 		TimeMode.REMAINING_TRACK:
-			pass
+			var remaining_track_time = $AudioStreamPlayer.stream.get_length() - elapsed_time
+			minutes = int(remaining_track_time / 60)
+			seconds = int(remaining_track_time) % 60
 		TimeMode.REMAINING_TOTAL:
-			total_time -= elapsed_time
-			minutes = int(total_time / 60)
-			seconds = int(total_time) % 60
+			var total_remaining_time = get_total_remaining_time()
+			total_remaining_time -= elapsed_time
+			minutes = int(total_remaining_time / 60)
+			seconds = int(total_remaining_time) % 60
 
 	$time_label.text = "%02d        %02d" % [minutes, seconds]
 
-func _on_next_button_pressed():
-	previous_track_index = current_track_index
+func _on_play_button_pressed():
+	if not $AudioStreamPlayer.is_playing():
+		if current_playback_mode == PlaybackMode.PROGRAM:
+			if program_list.size() > 0:
+				current_program_position = 0  # Start from the beginning of the program list
+				load_track(program_list[current_program_position])
+				$AudioStreamPlayer.play()
+			else:
+				print("Program list is empty")
+				return
+		else:
+			load_track(current_track_index)
+			$AudioStreamPlayer.play()
 
+func _on_pause_button_pressed():
+	$AudioStreamPlayer.stream_paused = !$AudioStreamPlayer.stream_paused
+
+func _on_stop_button_pressed():
+	$AudioStreamPlayer.stop()
+	$time_label.text = "00      00"
+
+func _on_next_button_pressed():
 	match current_playback_mode:
 		PlaybackMode.NORMAL:
 			if current_track_index < (track_files.size()) - 1:
@@ -213,16 +258,20 @@ func _on_next_button_pressed():
 			current_track_index = next_track
 			played_tracks.append(current_track_index)
 		PlaybackMode.PROGRAM:
-			var current_program_index = program_list.find(current_track_index)
-			if current_program_index < program_list.size() - 1:
-				current_track_index = program_list[current_program_index + 1]
+			if current_program_position < program_list.size() - 1:
+				current_program_position += 1
+				load_track(program_list[current_program_position])
+				$AudioStreamPlayer.play()
 			else:
 				if repeat_mode == RepeatMode.REPEAT_ALL:
-					current_track_index = program_list[0]
+					current_program_position = 0
+					load_track(program_list[current_program_position])
+					$AudioStreamPlayer.play()
 				else:
 					print("End of program list reached.")
 					$AudioStreamPlayer.stop()
 					return
+			return  # Exit early since load and play are already called
 
 	load_track(current_track_index)
 	$AudioStreamPlayer.play()
@@ -250,34 +299,18 @@ func _on_previous_button_pressed():
 					print("No previous track in shuffle history.")
 					return
 			PlaybackMode.PROGRAM:
-				var current_program_index = program_list.find(current_track_index)
-				if current_program_index > 0:
-					current_track_index = program_list[current_program_index - 1]
+				if current_program_position > 0:
+					current_program_position -= 1
+					load_track(program_list[current_program_position])
+					$AudioStreamPlayer.play()
 				else:
 					print("Start of program list reached.")
 					return
+				return  # Exit early since load and play are already called
 
 	load_track(current_track_index)
 	$AudioStreamPlayer.play()
 	update_track_visibility()
-
-func _on_play_button_pressed():
-	if not $AudioStreamPlayer.is_playing():
-		if current_playback_mode == PlaybackMode.PROGRAM:
-			if program_list.size() > 0:
-				current_track_index = program_list[0]
-			else:
-				print("Program list is empty")
-				return
-		load_track(current_track_index)
-		$AudioStreamPlayer.play()
-
-func _on_stop_button_pressed():
-	$AudioStreamPlayer.stop()
-	$time_label.text = "00        00"
-
-func _on_pause_button_pressed():
-	$AudioStreamPlayer.stream_paused = !$AudioStreamPlayer.stream_paused
 
 func _on_seek_backward_button_button_down():
 	seeking_backward = true
@@ -290,6 +323,19 @@ func _on_seek_forward_button_button_down():
 
 func _on_seek_forward_button_button_up():
 	seeking_forward = false
+
+func _on_audio_stream_player_finished():
+	if repeat_mode == RepeatMode.REPEAT_ONE:
+		load_track(current_track_index)
+		$AudioStreamPlayer.play()
+	else:
+		_on_next_button_pressed()
+	update_track_visibility()
+
+func _on_time_button_pressed():
+	time_mode = (time_mode + 1) % TimeMode.size() as TimeMode
+	update_elapsed_time_label()
+	$time_remaining_orb.visible = (time_mode != TimeMode.ELAPSED)
 
 func _on_repeat_button_pressed():
 	repeat_mode = ((repeat_mode + 1) % RepeatMode.size()) as RepeatMode
@@ -304,55 +350,24 @@ func update_repeat_label():
 		RepeatMode.REPEAT_ALL:
 			print("REPEAT ALL")
 
-func turn_off_orb_for_finished_track(track_index):
-	var node_path = "trackNumbers/track_%02d_button/track_%02d_orb" % [track_index + 1, track_index + 1]
-	var orb = get_node_or_null(node_path)
-	if orb: orb.visible = false
-
-func turn_on_orb_for_previous_track(track_index):
-	var node_path = "trackNumbers/track_%02d_button/track_%02d_orb" % [track_index + 1, track_index + 1]
-	var orb = get_node_or_null(node_path)
-	if orb: orb.visible = true
-
-func turn_on_orb_for_current_track(track_index):
-	var node_path = "trackNumbers/track_%02d_button/track_%02d_orb" % [track_index + 1, track_index + 1]
-	var orb = get_node_or_null(node_path)
-	if orb: orb.visible = true  # Ensure the orb is visible for the current track
-
-func _on_audio_stream_player_finished():
-	var finished_track_index = current_track_index
-
-	if repeat_mode == RepeatMode.REPEAT_ONE:
-		load_track(finished_track_index)
-		$AudioStreamPlayer.play()
-	else:
-		_on_next_button_pressed()
-	
-	update_track_visibility()
-
-func _on_time_button_pressed():
-	time_mode = (time_mode + 1) % TimeMode.size() as TimeMode
-	update_elapsed_time_label()
-	$time_remaining_orb.visible = (time_mode != TimeMode.ELAPSED)
-
 func get_total_remaining_time() -> float:
 	var total_remaining_time = 0.0
-	if track_durations.size() == 0:
-		return total_remaining_time
 
-	for i in range(current_track_index, track_durations.size()):
-		total_remaining_time += track_durations[i]
+	if current_playback_mode == PlaybackMode.PROGRAM:
+		for i in range(current_program_position, program_list.size()):
+			var track_idx = program_list[i]
+			if track_idx < track_durations.size():
+				total_remaining_time += track_durations[track_idx]
+	elif current_playback_mode == PlaybackMode.SHUFFLE:
+		for i in range(current_shuffle_index + 1, shuffle_order.size()):
+			var track_idx = shuffle_order[i]
+			if track_idx not in played_tracks and track_idx < track_durations.size():
+				total_remaining_time += track_durations[track_idx]
+	else:
+		for i in range(current_track_index, track_durations.size()):
+			total_remaining_time += track_durations[i]
 
 	return total_remaining_time
-
-func calculate_track_durations():
-	track_durations.clear()
-	for track_path in track_files:
-		var duration = get_track_duration(track_path)
-		if duration >= 0:
-			track_durations.append(duration)
-		else:
-			track_durations.append(0)
 
 func get_track_duration(path: String) -> float:
 	var music_stream = load_mp3(path)
@@ -360,36 +375,26 @@ func get_track_duration(path: String) -> float:
 		return music_stream.get_length()
 	return -1
 
-func _on_continue_button_pressed():
-	current_playback_mode = PlaybackMode.NORMAL
-	repeat_mode = RepeatMode.NO_REPEAT
-	
-	var currently_playing_track = track_files[current_track_index]
-	current_track_index = original_order.find(currently_playing_track)
-
-	track_files = original_order.duplicate()
-
-	update_elapsed_time_label()
-	update_track_visibility()
-
-func _on_exit_button_pressed():
-	get_tree().quit()
-
-func _input(_event):
-	if Input.is_action_just_pressed("ui_select"):
-		$VisualiserLayer/VisualiserRect.visible = !$VisualiserLayer/VisualiserRect.visible
-
-func _on_track_button_pressed(track_number: int):
-	var track_index = track_number - 1
-	
-	# Check if the track index is valid and a track is loaded at this index
-	if track_index < track_files.size():
-		current_track_index = track_index
+func _on_shuffle_button_pressed():
+	if current_playback_mode != PlaybackMode.SHUFFLE:
+		current_playback_mode = PlaybackMode.SHUFFLE
+		shuffle_order = range(track_files.size())
+		shuffle_order.shuffle()
+		played_tracks.clear()
+		shuffle_history.clear()
+		current_shuffle_index = 0
+		current_track_index = shuffle_order[current_shuffle_index]
+		shuffle_history.append(current_track_index)
+		played_tracks.append(current_track_index)
+		
 		load_track(current_track_index)
 		$AudioStreamPlayer.play()
-		update_track_visibility()
 	else:
-		print("No track loaded at this position.")
+		current_playback_mode = PlaybackMode.NORMAL
+		played_tracks.clear()
+	
+	print("Shuffle mode: ", current_playback_mode == PlaybackMode.SHUFFLE)
+	update_track_visibility()
 
 func get_next_shuffle_track() -> int:
 	if played_tracks.size() >= track_files.size():
@@ -417,61 +422,54 @@ func get_next_shuffle_track() -> int:
 	shuffle_history.append(next_track)
 	return next_track
 
-func _on_shuffle_button_pressed():
-	shuffle = !shuffle
-	if shuffle:
-		current_playback_mode = PlaybackMode.SHUFFLE
-		shuffle_order = range(track_files.size())
-		shuffle_order.shuffle()
-		played_tracks.clear()
-		shuffle_history.clear()
-		current_shuffle_index = 0  # Start from the first track in the shuffled order
-		current_track_index = shuffle_order[current_shuffle_index]
-		shuffle_history.append(current_track_index)
-		played_tracks.append(current_track_index)
-		
-		# Load and play the first track in the shuffled order
-		load_track(current_track_index)
-		$AudioStreamPlayer.play()
-	else:
-		current_playback_mode = PlaybackMode.NORMAL
-		track_files = original_order.duplicate()
-		played_tracks.clear()
-		# Optionally, you can stop playback when exiting shuffle mode
-		# $AudioStreamPlayer.stop()
-	
-	print("Shuffle: ", shuffle)
-	update_track_visibility()
-
-func add_to_program_list(track_index: int):
-	if track_index not in program_list:
-		program_list.append(track_index)
-		update_track_visibility()
-	print("Program list: ", program_list)
-
 func _on_program_button_pressed():
 	if current_playback_mode != PlaybackMode.PROGRAM:
 		current_playback_mode = PlaybackMode.PROGRAM
 		program_list.clear()
+		current_program_position = 0  # Reset position
 		update_track_visibility()
 		print("Program mode activated")
 	else:
 		current_playback_mode = PlaybackMode.NORMAL
 		program_list.clear()
-		reset_normal_mode_visibility()
-		print("NORMAL MODE")
+		update_track_visibility()
+		print("Normal mode")
 
-func reset_normal_mode_visibility():
-	var track_buttons = get_tree().get_nodes_in_group("track_buttons")
-	for i in range(track_buttons.size()):
-		var track_button = track_buttons[i]
-		var orb_name = "track_%02d_orb" % (i + 1)
-		var orb = track_button.get_node_or_null(orb_name)
-		
-		if orb:
-			if i < track_files.size():
-				orb.visible = i >= current_track_index
-			else:
-				orb.visible = false
-	
+func add_to_program_list(track_index: int):
+	# Allow duplicates in program list
+	program_list.append(track_index)
 	update_track_visibility()
+	print("Program list: ", program_list)
+
+func _on_continue_button_pressed():
+	current_playback_mode = PlaybackMode.NORMAL
+	repeat_mode = RepeatMode.NO_REPEAT
+
+	var currently_playing_track = track_files[current_track_index]
+	current_track_index = original_order.find(currently_playing_track)
+
+	update_elapsed_time_label()
+	update_track_visibility()
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+func _input(_event):
+	if Input.is_action_just_pressed("ui_select"):
+		$VisualiserLayer/VisualiserRect.visible = !$VisualiserLayer/VisualiserRect.visible
+
+func _on_track_button_pressed(track_number: int):
+	var track_index = track_number - 1
+
+	if track_index < track_files.size():
+		if current_playback_mode == PlaybackMode.PROGRAM:
+			# In program mode, add track to program list (allow duplicates)
+			add_to_program_list(track_index)
+		else:
+			# In normal or shuffle mode, play the track directly
+			current_track_index = track_index
+			load_track(current_track_index)
+			$AudioStreamPlayer.play()
+			update_track_visibility()
+	else:
+		print("No track loaded at this position.")
